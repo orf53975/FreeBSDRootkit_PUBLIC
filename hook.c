65e736096b753da902bab5c5d3ab61720fbdca78
Lachlan Jones <rid.3r@hotmail.com>
2018-09-26 18:10:52 +1000

    stub file for hooking syscalls

---
+++ hook.c
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/proc.h>
+#include <sys/module.h>
+#include <sys/sysent.h>
+#include <sys/kernel.h>
+#include <sys/systm.h>
+#include <sys/syscall.h>
+#include <sys/sysproto.h>
+
+// kldstat system call hook
+static int kldstat_hook(struct thread *td, void *syscall_args) {
+    
+}

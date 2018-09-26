0ab9959d0002f6de1bf41544a9887b7853ff0e19
B3NNY <b.lichtman@student.unsw.edu.au>
2018-09-26 15:02:33 +1000

    added basic functions - empty

---
+++ rootkit.c
@@ -0,0 +1,49 @@
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/proc.h>
+#include <sys/module.h>
+#include <sys/sysent.h>
+#include <sys/kernel.h>
+#include <sys/systm.h>
+#include <sys/syscall.h>
+#include <sys/sysproto.h>
+#include <sys/malloc.h>
+
+#include <sys/linker.h>
+#include <sys/lock.h>
+#include <sys/mutex.h>
+
+#include <vm/vm.h>
+#include <vm/vm_page.h>
+#include <vm/vm_map.h>
+
+#include <dirent.h>
+
+/* The function called at load/unload. */
+static int
+load(struct module *module, int cmd, void *arg)
+{
+	int error = 0;
+
+	switch (cmd) {
+	case MOD_LOAD:
+		uprintf("System call loaded at offset %d.\n", offset);
+		break;
+
+	case MOD_UNLOAD:
+		uprintf("System call unloaded from offset %d.\n", offset);
+		break;
+
+	default:
+		error = EOPNOTSUPP;
+		break;
+	}
+}
+
+static moduledata_t rootkit_mod = {
+	"rootkit",		/* module name */
+	load,			/* event handler */
+	NULL			/* extra data */
+};
+
+DECLARE_MODULE(rootkit, rootkit_mod, SI_SUB_DRIVERS, SI_ORDER_MIDDLE);

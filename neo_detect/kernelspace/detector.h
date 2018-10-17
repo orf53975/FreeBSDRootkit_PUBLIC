4dc2524131991d9e9a1deda5c1eda19aa16498aa
B3NNY <b.lichtman@student.unsw.edu.au>
2018-10-17 20:45:44 +1100

    New detector architecture created

---
+++ neo_detect/kernelspace/detector.h
@@ -0,0 +1,39 @@
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/systm.h>
+#include <sys/param.h>
+#include <sys/proc.h>
+#include <sys/module.h>
+#include <sys/sysent.h>
+#include <sys/kernel.h>
+#include <sys/systm.h>
+#include <sys/syscall.h>
+#include <sys/syscallsubr.h>
+#include <sys/sysproto.h>
+#include <sys/malloc.h>
+#include <sys/linker.h>
+#include <sys/lock.h>
+#include <sys/mutex.h>
+#include <sys/sx.h>
+#include <sys/dirent.h>
+#include <sys/cdefs.h>
+#include <sys/resourcevar.h>
+#include <sys/fcntl.h>
+#include <sys/errno.h>
+
+#include <sys/systm.h>
+#include <sys/pcpu.h>
+#include <sys/fcntl.h>
+#include <sys/file.h>
+
+#define LINKER_FILE "detector.ko"
+#define MODULE_NAME "detector"
+
+/* The system call's arguments. */
+struct detector_args {
+	int command;
+	char ** args;
+};
+
+int run_all_tests(struct thread * td, struct detector_args * uap);
---
+++ neo_detect/kernelspace/functionality.c
@@ -0,0 +1,5 @@
+#include "detector.h"
+
+int run_all_tests(struct thread * td, struct detector_args * uap) {
+	return 0;
+}

